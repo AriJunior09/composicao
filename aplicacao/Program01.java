6029e7da4ad58d358d0f6fa40853f6bf5c71af4b
Ari Júnior <arioliveirajuniorjose@gmail.com>
2023-11-10 21:45:50 -0300

    Correção de textos

--- aplicacao/Program01.java
+++ aplicacao/Program01.java
@@ -24,7 +24,7 @@ public class Program01 {
 		String nomeTrabalhador = sc.nextLine();
 		System.out.print("Nivel: ");
 		String nivelTrabalhador = sc.nextLine();
-		System.out.print("Base salarial: ");
+		System.out.print("Base salarial: R$ ");
 		double baseSalario = sc.nextDouble();
 		Trabalhador trabalhador = new Trabalhador(nomeTrabalhador, NivelTrabalhador.valueOf(nivelTrabalhador), baseSalario, new Departamento(nomeDepartamento));
 		
@@ -32,10 +32,10 @@ public class Program01 {
 		int n = sc.nextInt();
 		
 		for (int i=1; i<=n; i++) {
-			System.out.println("Entre com o contrato #" + i + " dados:");
+			System.out.println("Entre com os dados, contrato #" + i + ":");
 			System.out.print("Data (DD/MM/AAAA): ");
 			Date contratoData = sdf.parse(sc.next());
-			System.out.print("Valor por hora: ");
+			System.out.print("Valor por hora: R$ ");
 			double valorPorHora = sc.nextDouble();
 			System.out.print("Duração (horas): ");
 			int horas = sc.nextInt();
@@ -44,13 +44,13 @@ public class Program01 {
 		}
 		
 		System.out.println();
-		System.out.print("Entre com o mês e o ano que deseja consutar a renda (MM/AAAA): ");
+		System.out.print("Entre com o mês e o ano que deseja consultar a renda (MM/AAAA): ");
 		String mes_e_ano = sc.next();
 		int mes = Integer.parseInt(mes_e_ano.substring(0, 2));
 		int ano = Integer.parseInt(mes_e_ano.substring(3));
 		System.out.println("Nome: " + trabalhador.getNome());
 		System.out.println("Departamento: " + trabalhador.getDepartamento().getNome());
-		System.out.println("Renda para: " + mes_e_ano + ": " + String.format("%.2f", trabalhador.renda(ano, mes)));
+		System.out.println("Renda para: " + mes_e_ano + ": R$ " + String.format("%.2f", trabalhador.renda(ano, mes)));
 		
 		sc.close();
 	}
